0d7640c454dffb29950158a0cbc2b50d4a4a04fa
Mohammed Bamhraz <mohmedbamhrz@gmail.com>
2018-07-30 15:18:48 +0300

    - fix error on macOs

--- Screens/ExploreScreen.js
+++ Screens/ExploreScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TextInput, Platform, StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
 
 class ExploreScreen extends Component {
   constructor(props) {
@@ -8,11 +9,29 @@ class ExploreScreen extends Component {
     };
   }
 
+  componentWillMount() {
+    this.startHeaderHeight = 80
+    if(Platform.OS == 'android') {
+      this.startHeaderHeight = 100 + StatusBar.currentHeight
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text> textInComponent </Text>
-      </View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <View style={styles.inputParent}>
+              <Icon name='ios-search' size={20} />
+              <TextInput
+                underlineColorAndroid='transparent'
+                placeholder="Try Jeddah"
+                placeholderTextColor="grey"
+                style={styles.textInput} />
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
     );
   }
 }
@@ -22,8 +41,27 @@ export default ExploreScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    height: 80,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: '#dddddd'
+  },
+  inputParent: {
+    flexDirection: 'row',
+    padding: 10,
+    backgroundColor: 'white',
+    marginHorizontal: '20',
+    shadowOffset: { width: 0, height: 0 },
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    elevation: 1,
+    marginTop: Platform.OS == 'android' ? 30 : null
+  },
+  textInput: {
+    flex: 1,
+    fontWeight: '700',
+    backgroundColor: 'white'
+  }
 });
